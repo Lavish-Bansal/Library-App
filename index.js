eeeae20565d848b106680976703098e9e75de6fd
Lavish Bansal <85956252+Lavish-Bansal@users.noreply.github.com>
2021-12-10 18:45:58 +0530

    Add files via upload
    Initial commit

---
+++ index.js
@@ -0,0 +1,78 @@
+function Book(name, author, type) {
+  this.name = name;
+  this.author = author;
+  this.type = type;
+}
+
+function Display() {}
+
+Display.prototype.add = function (book) {
+  tableBody = document.getElementById("tableBody");
+  let uistring = `<tr>
+                        <td>${book.name}</td>
+                        <td>${book.author}</td>
+                        <td>${book.type}</td>
+                    </tr>`;
+
+  tableBody.innerHTML += uistring;
+};
+
+Display.prototype.clear = function () {
+  let libraryForm = document.getElementById("libraryForm");
+  libraryForm.reset();
+};
+
+Display.prototype.validate = function (book) {
+  if (book.name.length < 2 || book.author.length < 2) {
+    return false;
+  } else {
+    return true;
+  }
+};
+
+Display.prototype.show = function (type, displayMessage) {
+  let message = document.getElementById("message");
+  message.innerHTML = `<div class="alert alert-${type} alert-dismissible fade show" role="alert">
+                            <strong>Message:</strong> ${displayMessage}
+    //                     </div>`;
+  setTimeout(() => {
+    message.innerHTML = "";
+  }, 2000);
+};
+
+let libraryForm = document.getElementById("libraryForm");
+libraryForm.addEventListener("submit", libraryFormSubmit);
+
+function libraryFormSubmit(e) {
+  console.log("You have submitted the form");
+  let name = document.getElementById("bookName").value;
+  let author = document.getElementById("author").value;
+
+  let type;
+  let fiction = document.getElementById("fiction");
+  let programming = document.getElementById("programming");
+  let comedy = document.getElementById("comedy");
+
+  if (fiction.checked) {
+    type = fiction.value;
+  } else if (programming.checked) {
+    type = programming.value;
+  } else if (comedy.checked) {
+    type = comedy.value;
+  }
+
+  let book = new Book(name, author, type);
+  console.log(book);
+
+  let display = new Display();
+
+  if (display.validate(book)) {
+    display.add(book);
+    display.clear();
+    display.show("success", "Your book has been successfully added");
+  } else {
+    display.show("danger", "Sorry This book cannot be added");
+  }
+
+  e.preventDefault();
+}
